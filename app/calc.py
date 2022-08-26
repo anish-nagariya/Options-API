43a64e1c1a5c7e0ef249242471dc2c47556b306c
Anish Nagariya <anishnagariya07@gmail.com>
2022-08-26 07:26:59 -0400

    Update calc.py

--- app/calc.py
+++ app/calc.py
@@ -1,9 +1,14 @@
+import math
 from datetime import datetime, timedelta, timezone
 import time
 import asyncio
 import aiohttp
 import yfinance as yf
-from multiprocessing import Pool
+import nest_asyncio
+# from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from apscheduler.triggers.cron import CronTrigger
+
+nest_asyncio.apply()
 
 tickers = ["SPY", "QQQ", "TSLA", "AMZN", "NFLX", "META", "AAPL", "MSFT", "NVDA", "AMD", "TWTR", "NIO", "GOOGL", "SHOP",
            "BA", "ROKU", "DIA", "MRNA", "BABA", "DIS", "BAC", "JPM", "F", "AMC", "GME", "INTC", "UBER", "SNAP", "SQ",
@@ -57,21 +62,39 @@ def calc(ticker):
     return ticker, round(result['Puts'] / result['Calls'], 3)
 
 
-if __name__ == "__main__":
-    p = Pool(8)
-    results = {}
-    for t in tickers: results[t] = []
-    reset = False
-    if reset:
-        f = open('pcvr.csv', 'w')
-        f.write("time,")
-        for t in tickers:f.write(f'{t},')
-        f.write('\n')
-        f.close()
-    days = 0
-    prev = 0
-    while datetime.now().second > 5: time.sleep(1)
-    while True:
+results = {}
+for t in tickers: results[t] = []
+
+
+def run(stocks):
+    async def calculate(stock):
+        global results
+        r = calc(stock)
+        results[r[0]] = [r[1], (datetime.now(timezone.utc) - timedelta(hours=5)).strftime('%Y-%m-%d %H:%M')]
+
+    async def calc_all():
+        tasks = []
+        for s in stocks:
+            tasks.append(asyncio.create_task(calculate(s)))
+        await asyncio.wait(tasks)
+
+    asyncio.get_event_loop().run_until_complete(calc_all())
+
+
+s = time.time()
+reset = True
+if reset:
+    f = open('pcvr.csv', 'w')
+    f.write("time,")
+    for t in tickers: f.write(f'{t},')
+    f.write('\n')
+    f.close()
+days = 0
+prev = 0
+while datetime.now().second > 5: time.sleep(1)
+while True:
+    c = datetime.now(timezone.utc).strftime('%H%M')
+    while '1330' < c < '2000' and datetime.now().weekday() < 5:
         if datetime.now().day != prev:
             days += 1
             prev = datetime.now().day
@@ -82,20 +105,24 @@ if __name__ == "__main__":
             for t in tickers: f.write(f'{t},')
             f.write('\n')
             f.close()
+
         start_ = time.time()
         f = open('pcvr.csv', 'a')
-        # print('Running...')
+        print('Running...')
         f.write(f"{(datetime.now(timezone.utc) - timedelta(hours=5)).strftime('%Y-%m-%d %H:%M')},")
-        resp = p.map(calc, tickers)
-        for r in resp:
-            results[r[0]].append([r[1], (datetime.now(
-                timezone.utc) - timedelta(hours=5)).strftime('%Y-%m-%d %H:%M')])
-            f.write(f'{r[1]},')
+        run(tickers)
+        print(results)
+        for t in tickers:
+            if math.isnan(float(results[t][0])):
+                f.write(f'-1, ')
+            else:
+                f.write(f'{results[t][0]},')
         f.write('\n')
         f.close()
         # print(results)
         # print('Time Taken {}'.format(time.time() - start_))
         # print('\n')
+
         try:
             time.sleep(60 - (time.time() - start_))
         except Exception:
